d21862189ca4fa5bf11d7676a33352e0ebb56af2
Somto96 <motivatedsomt@gmail.com>
2019-11-19 16:30:48 +0100

    dynamic rendering of pages

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -7,6 +7,31 @@ const app = express();
 
 app.use(express.static(publicDir));
 
+app.set('view engine', 'hbs');
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: "Somto Ezerioha"
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me',
+        name: 'Somto Ezerioha',
+        description: "Hi, I'm Somtochukwu Ezerioha. A full stack-developer with over 3 years of experience."
+    });
+});
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        disclaimer: 'This is our help page',
+        copyright: 'Somto Ezerioha @ 2019',
+        description: "Oops! this page is still under construction. Check back later."
+    });
+});
+
 app.get('/weather', (req, res) => {
     res.send({
         forecast: 'Sunny',
